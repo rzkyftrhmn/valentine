e1b4e8d8598181704f45cf4d72293578bfbc7243
rzkyftrhmn <faturohmanrizky36@gmail.com>
2026-02-14 17:15:50 +0700

    update foto jokai

--- app/page.tsx
+++ app/page.tsx
@@ -12,17 +12,6 @@ export default function Home() {
     '/2.jpeg',
     '/3.jpeg',
     '/4.jpeg',
-    '/5.jpeg',
-    '/6.jpeg',
-    '/7.jpeg',
-    '/8.jpeg',
-    '/9.jpeg',
-    '/10.jpeg',
-    '/11.jpeg',
-    '/12.jpeg',
-    '/13.jpeg',
-    '/14.jpeg',
-    '/15.jpeg',
   ];
 
   return (
